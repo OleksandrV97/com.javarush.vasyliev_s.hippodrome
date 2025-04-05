bf26fb2d1e6e42a2f091489580496dc4b2d94602
OleksandrV97 <vasyliev1work@gmail.com>
2025-04-05 16:22:22 +0100

    Commit Third

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class HippodromeTest {
     @Test
@@ -18,9 +20,33 @@ public class HippodromeTest {
         for (int i = 1; i < 30; i++) {
             horses.add(new Horse("" + i,i,i));
         }
-
         Hippodrome hippodrome = new Hippodrome(horses);
         assertEquals(horses, hippodrome.getHorses());
+    }
+
+    @Test
+    public void move() {
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            horses.add(mock(Horse.class));
+        }
+        new Hippodrome(horses).move();
+
+        for (Horse horse : horses) {
+            verify(horse).move();
+        }
+    }
+
+    @Test
+    public void getWinner() {
+        Horse horse1 = new Horse("hor1",1, 4);
+        Horse horse2 = new Horse("hor2",1, 3);
+        Horse horse3 = new Horse("hor3",1, 2);
+        Horse horse4 = new Horse("hor4",1, 1);
+        Hippodrome hippodrome = new Hippodrome(List.of(horse1, horse2, horse3, horse4));
+
+        assertEquals(horse1, hippodrome.getWinner());
+
     }
 
 }
--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -12,16 +12,16 @@ import static org.mockito.Mockito.mockStatic;
 public class HorseTest {
 
     @Test
-    public void nullNameException(){
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Horse(null,1,1));
+    public void nullNameException() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 1, 1));
 
         assertEquals("Name cannot be null.", e.getMessage());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", "  ", "\t\t", "\n\n\n\n\n\n"})
-    public void blankNameException(String name){
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Horse(name, 1,1));
+    public void blankNameException(String name) {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Horse(name, 1, 1));
 
         assertEquals("Name cannot be blank.", e.getMessage());
     }
@@ -30,27 +30,27 @@ public class HorseTest {
     public void getName() throws NoSuchFieldException, IllegalAccessException {
         Horse horse = new Horse("white", 1, 1);
 
-       Field name = Horse.class.getDeclaredField("name");
-       name.setAccessible(true);
-       String nameValue = (String) name.get(horse);
-       assertEquals("white",nameValue);
+        Field name = Horse.class.getDeclaredField("name");
+        name.setAccessible(true);
+        String nameValue = (String) name.get(horse);
+        assertEquals("white", nameValue);
     }
 
     @Test
     public void getSpeed() {
-        Horse horse = new Horse("white", 321,1);
-        assertEquals(321,horse.getSpeed());
+        Horse horse = new Horse("white", 321, 1);
+        assertEquals(321, horse.getSpeed());
     }
 
     @Test
     public void getDistance() {
-        Horse horse = new Horse("white",1, 345);
+        Horse horse = new Horse("white", 1, 345);
         assertEquals(345, horse.getDistance());
     }
 
     @Test
     public void zeroDistanceDefault() {
-        Horse horse = new Horse("white",1);
+        Horse horse = new Horse("white", 1);
         assertEquals(0, horse.getDistance());
     }
 
@@ -64,5 +64,17 @@ public class HorseTest {
         }
     }
 
+    @ParameterizedTest
+    @ValueSource(doubles = {0.1, 0.2, 0.5, 0.9, 1.0})
+    void move(double random) {
+        try (MockedStatic<Horse> mockedStatic = mockStatic(Horse.class)) {
+            Horse horse = new Horse("white", 321, 345);
+            mockedStatic.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(random);
+
+            horse.move();
+            assertEquals(345 + 321 + random, horse.getDistance());
+        }
+
+    }
 }
 
